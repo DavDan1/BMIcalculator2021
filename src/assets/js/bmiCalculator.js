55cb277f47b8bb93e7d4842e31a35d86c98ab5c2
DavDan1 <david-danielyan@hotmail.com>
2021-04-15 11:37:00 +0200

    added toFix method in order to have two decimals of the result

--- src/assets/js/bmiCalculator.js
+++ src/assets/js/bmiCalculator.js
@@ -1,5 +1,5 @@
 const  calculateMetricBMI = (weight, height) => {
 	let  result
-	result = weight / ((height / 100) * (height / 100))
+	result = weight / ((height / 100) * (height / 100)).toFixed(2)
 	return  result
 }
