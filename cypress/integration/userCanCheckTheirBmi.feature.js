6269bc08fe07dcbc06c1254dcdcc0d76f2d6e904
DavDan1 <david-danielyan@hotmail.com>
2021-04-15 11:19:15 +0200

    added test that calculates BMI

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -6,4 +6,7 @@ describe("User can check their Bmi", () => {
     cy.get("#height").type("186");
     cy.get('#Calculate').click()
   });
+  it('displays a BMI value of 26.01', () => {
+    cy.get('#results').should('contain', '26.01')
+})
 });
