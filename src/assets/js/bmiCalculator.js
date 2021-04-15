b895832ff30b940b66496ec229cb17931810eb43
DavDan1 <david-danielyan@hotmail.com>
2021-04-15 11:51:59 +0200

    added styles,  fixed issue with toFix

--- src/assets/js/bmiCalculator.js
+++ src/assets/js/bmiCalculator.js
@@ -1,7 +1,7 @@
 const  calculateMetricBMI = (weight, height) => {
 	let  result
-	result = weight / ((height / 100) * (height / 100)).toFixed(2)
-	return  result
+	result = weight / ((height / 100) * (height / 100));
+	return  result.toFixed(2)
 }
 const  getBMIClassification = (value) => {
 	if (value < 18.5){
