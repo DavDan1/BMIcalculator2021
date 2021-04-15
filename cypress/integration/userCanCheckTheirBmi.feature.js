00ca9e8f8750f1b8460ec31850621ffc6922903f
DavDan1 <david-danielyan@hotmail.com>
2021-04-15 11:16:48 +0200

    added two inputs in my HTML code and added test to run them, all is green

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,6 +1,9 @@
-describe('User can check their Bmi', () => {
-	it('user can enter weight and height', () => {
-		cy.visit('http://localhost:3001')
-        cy.contains('BMI Calculator')
-	})
-})
+describe("User can check their Bmi", () => {
+  it("user can enter weight and height", () => {
+    cy.visit("http://localhost:3001");
+    cy.contains("BMI Calculator");
+    cy.get("#weight").type("90");
+    cy.get("#height").type("186");
+    cy.get('#Calculate').click()
+  });
+});
